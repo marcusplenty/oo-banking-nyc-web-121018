a1d0beb45de31a305deba73a15e758e5ed1af78a
Marcus Plenty <marcusplenty@gmail.com>
2018-12-18 01:29:07 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,8 +1,10 @@
 class Transfer
-  attr_reader :sender , :receiver
-  def initialize(sender, receiver)
+  attr_reader :sender , :receiver,
+  attr_accessor :amount
+  def initialize(sender, receiver, amount)
     @sender = sender
     @receiver = receiver
+    @amount = amount
     @status = "pending"
   end 
 end
