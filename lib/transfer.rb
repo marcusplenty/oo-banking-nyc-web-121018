b9bad67246e7f41cf3831cadf68740cb316488ac
Marcus Plenty <marcusplenty@gmail.com>
2018-12-18 01:47:19 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,6 +16,7 @@ class Transfer
       @receiver.balance += @amount
       @status = "complete"
     else 
+      @status = "rejected"
       return "Transaction rejected. Please check your account balance."
     end 
     
